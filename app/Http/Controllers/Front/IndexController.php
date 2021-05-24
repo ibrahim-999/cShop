2677cd74d45866058f3a41ee9425c65e8b41526f
ibrahim khalaf <ibrahimkhalaf99@gmail.com>
2021-05-24 16:41:38 +0200

    pass new products

--- app/Http/Controllers/Front/IndexController.php
+++ app/Http/Controllers/Front/IndexController.php
@@ -11,12 +11,19 @@ class IndexController extends Controller
     public function index()
     {
         // Get Featured Items
-        $featuredItemsCount = Product::where('is_featured','yes')->count();
-        $featuredItems = Product::where('is_featured','yes')->get()->toArray();
+        $featuredItemsCount = Product::where('is_featured','yes')->where('status',1)->count();
+        $featuredItems = Product::where('is_featured','yes')->where('status',1)->get()->toArray();
         /*$featuredItems = json_decode(json_encode($featuredItems),true);
         echo "<pre>";print_r($featuredItems);die;*/
         $featuredItemsChunk = array_chunk($featuredItems,4);
+
+        //Get Latest Products
+        $newProducts = Product::orderBy('id','Desc')->where('status',1)->limit(6)->get()->toArray();
+        //$newProducts = json_decode(json_encode($newProducts),true);
+        //echo "<pre>";print_r($featuredItems);die;
+
+
         $page_name = "index";
-        return view('front.index')->with(compact('page_name','featuredItemsChunk','featuredItemsCount'));
+        return view('front.index')->with(compact('page_name','featuredItemsChunk','featuredItemsCount','newProducts'));
     }
 }
