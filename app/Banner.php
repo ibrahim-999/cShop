05bc7ed528df647698ef86f72bac541b42386594
ibrahim khalaf <ibrahimkhalaf99@gmail.com>
2021-06-21 09:29:52 +0200

    pass-banners

--- app/Banner.php
+++ app/Banner.php
@@ -4,5 +4,10 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 class Banner extends Model
 {
-
+    public static function getBanners()
+    {
+        $getBanners = Banner::where('status',1)->get()->toArray();
+        //dd($getBanners); die;
+        return $getBanners;
+    }
 }
